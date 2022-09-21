ee0af19834d24cab3f899a6c2bd3c94589d7fac8
이시현 <antoliny@isihyeons-MacBook-Air.local>
2022-09-21 09:38:27 +0900

    Connecting the navbar area template on the homepage

--- search/views.py
+++ search/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def homepage(request):
-  return render(request, 'search/index.html')
+  return render(request, 'search/base_navbar/build/base_navbar.html')
