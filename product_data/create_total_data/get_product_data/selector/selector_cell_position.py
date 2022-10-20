e9563cd1978f804033294f9f88afc69babd1c5c2
antoliny <antoliny0919@naver.com>
2022-10-20 10:53:05 +0900

    valid cell data find codenum conditional expression update

--- product_data/create_total_data/get_product_data/selector/selector_cell_position.py
+++ product_data/create_total_data/get_product_data/selector/selector_cell_position.py
@@ -1,12 +1,16 @@
 from collections.abc import MutableSequence
 import openpyxl
 import get_product_data.selector.selector as selectors
+import re
+
 
 def set_data(data: openpyxl):
+  
   """
     셀 데이터의 __str__값에서 엑셀 행열위치를 추출
     해당 행열위치 데이터만 리턴
   """
+  
   cell_data = str(data)
   find_position = cell_data.find(".")
   row_column_position = cell_data[find_position+1:-1]
@@ -14,10 +18,13 @@ def set_data(data: openpyxl):
 
 
 def selector_cell(row_data: MutableSequence, name: str):
+  
   """
   유효한 행 데이터를 받아 순회하여
   선택자에 맞는 셀 데이터의 위치를 파악
   """
+  
+  expressions = re.compile(r'[\w]')
   valid_cell_position = {
     "코드번호": "",
     "품명": "",
@@ -33,7 +40,7 @@ def selector_cell(row_data: MutableSequence, name: str):
     
   for data in row_data:
     str_data = str(data.value).replace(" ","").replace("\n","")
-    
+    # print(data, str_data, type(data))
     if (str_data in selectors.name_selector):
       valid_cell_position["품명"] = set_data(data)
       
@@ -45,10 +52,18 @@ def selector_cell(row_data: MutableSequence, name: str):
       
     elif (str_data in selectors.attr_selector):
       valid_cell_position["속성"] = set_data(data)
-  
+    
   # 유효한 행 데이터가 아닐시(valid_cell_position데이터가 불충분)
   if (valid_cell_position["가격"] == []):
     return False
+  
+  # cj, 오뚜기, 청정원 --> 상품코드가 존재(품명 왼쪽셀)
+  if (name == 'cjfreshway' or name == '오뚜기' or name == 'daesang'):
+    column, row = expressions.findall(valid_cell_position["품명"])
+    codenum_column = chr(ord(column) - 1)
+    valid_cell_position["코드번호"] = codenum_column + row
+    
+    
   
   return valid_cell_position
   
