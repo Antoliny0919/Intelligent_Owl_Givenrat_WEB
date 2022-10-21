885564dfe0ef3aaaaf39aa09fd1d6bcc1ca641dc
antoliny <antoliny0919@naver.com>
2022-10-21 19:10:24 +0900

    change valid_cell_position key name and add max_row data

--- product_data/create_total_data/get_product_data/product_parsing.py
+++ product_data/create_total_data/get_product_data/product_parsing.py
@@ -1,6 +1,6 @@
 import openpyxl
+import re
 from collections.abc import MutableSequence, MutableMapping
-from pyclbr import Function
 import get_product_data.selector.selector_cell_position as find_position
 import get_product_data.selector.selector as selector_data
 
@@ -29,7 +29,7 @@ def find_valid_range(obj: openpyxl.Workbook, range: str, name: str) -> MutableMa
   유효범위를 순회하면서 사용자가 원하는 키워드에 맞는 셀 위치를 파악
   return: MutableMapping형 {키워드: 셀 위치}
   """
-  
+  find_max_row = re.compile(r'[\d]')
   for row_data in obj[range]:  # tuple 객체안에 A1 ~ Z1 행 데이터가 담김
     
     for data in row_data:
@@ -37,46 +37,12 @@ def find_valid_range(obj: openpyxl.Workbook, range: str, name: str) -> MutableMa
       cell_data = str(data.value).replace(" ", "")
       
       # 시트값들을 순회하며 제품명이 담긴 행 데이터 selector_cell 인자로 전달
-      if (cell_data in selector_data.name_selector):
+      if (cell_data in selector_data.name_selector):        
+        max_row = "".join(find_max_row.findall(range)[1:])
+        valid_range = find_position.selector_cell(row_data, name, max_row)
         
-        valid_range = find_position.selector_cell(row_data, name)
         if(valid_range == False):
           break
         return valid_range
 
 
-
-
-
-
-
-
-
-
-# 브랜드가 상단에 위치하지 않을 수도 있음 (브랜드) 브랜드가 시트에 존재하기도 함 --> 시트부터 체크하는게 좋을듯(가장 간편)
-# 브랜드 통합인 회사도 있음 --> 브랜드명이 없어도됨 --> 동원, cj, 청정원
-# 한 시트에 두개의 브랜드가 존재하기도 함
-# 돈가상사는 브랜드 파싱이 힘듬
-# 피아골미선씨도 불가능
-# 마리요리도 불가능
-# 세호 재호물산, 새우튀김 불가능
-# 세호 맛본 불가능
-# 해오름 닭꼬치는 단가가 여려개임(일반단가, 1등급, 1등급친환경) --> 일반단가로 설정
-# 웰담 닭세트 불가(10월 웰담 닭세트제품)
-# 유림에프에스 하늘구름 불가능
-# 유림에프에스 순수지기 불가능
-# 유림에프에스 스마일푸드 불가능
-# 용봉 백미담 불가능(엑셀 파일이 안열림)
-# brand_selector = ['허쉬', '요리와유', '소예푸드', '코주부', '문옥례', '산들맘', '굿브랜드스쿨', '아라원', '판다스토리', '들녘식품', '화과방', '주원' \
-#   '시원스쿨FOOD', 'MINIKER', '오 뗄', '마이디벨', '사세', '푸드스토리', '올바른', '한국전통식품', '착한애', '가보팜스', '명가푸드', '목우촌', '바다한줌'\
-#     '노고치농원', '천지해', '신미유부', 'SRC', '마리요리', '라망피자', '도담푸드', '또앤또', '코끼리푸드', '맛본오란다', '재호물산', '맥케인', '참앤참떡' \
-#       '매일유업', '삼립샤니', '던킨도너츠', '해두른어묵', '해오름', '에이치쿡', '씨엔에스푸드', '올그루만두', '보리가득', '이룸리테일', '푸딩스쿨', '굿프렌즈', 
-#       '자연웰', '사자표소스', '웰담', '싱싱플러스', '우리밀로', '천일식품', '루토사', '초록푸드', '급식대장', '지산푸드', '휴먼앤푸드', '쿠엔츠버킷', '제임스덕'\
-#         , '순수지기', '이가자연면', '돈지천', '초록나무', '빛고을청아']
-
-
-# attr_selector = ['상품정보', '식품설명', '함량', '성분', '원재료 및 함량', '성분 및 함량', '비고', '비고란', '제품특징 및 원재료함량', '제품 특이사항'\
-#   , '제품성분', '주요성분', '제품함량', '성분 및 특징', '성분 및 특이사항', '세부품명', '특징', '제품함량&특이사항', '특이사항', '개당중량', '성분표시', '성분 함량, 알러지'\
-#     , '제품 및 알러지유발성분', '주요성분', '제 품 주 요 성 분', '원재료 함량', '인증 및 특허', '주요 사항', '원재료명 및 함량', '함량 및 제원']
-
-# code_num_selector = ['상품코드']
--- product_data/create_total_data/get_product_data/selector/selector_cell_position.py
+++ product_data/create_total_data/get_product_data/selector/selector_cell_position.py
@@ -1,10 +1,10 @@
-from collections.abc import MutableSequence
+from collections.abc import MutableSequence, MutableMapping
 import openpyxl
 import get_product_data.selector.selector as selectors
 import re
 
 
-def set_data(data: openpyxl):
+def set_data(data: openpyxl) -> str:
   
   """
     셀 데이터의 __str__값에서 엑셀 행열위치를 추출
@@ -17,7 +17,7 @@ def set_data(data: openpyxl):
   return row_column_position
 
 
-def selector_cell(row_data: MutableSequence, name: str):
+def selector_cell(row_data: MutableSequence, name: str, max_row: str) -> MutableMapping:
   
   """
   유효한 행 데이터를 받아 순회하여
@@ -25,26 +25,30 @@ def selector_cell(row_data: MutableSequence, name: str):
   """
   
   expressions = re.compile(r'[\w]')
+  
+  # 데이터 형식
   valid_cell_position = {
-    "코드번호": "",
-    "품명": [],
-    "메인브랜드": "",
-    "서브브랜드": "",
-    "규격": "",
-    "가격": [],
-    "속성":"",
+    "code_num": "",
+    "name": [],
+    "main_brand": "",
+    "sub_brand": "",
+    "size": "",
+    "price": [],
+    "attribute":"",
+    "max_row": "",
   }
+  
   # big_brand --> 씨제이, 청정원, 오뚜기는 서브브랜드 X
-  valid_cell_position["메인브랜드"] = name
+  valid_cell_position["main_brand"] = name
+  valid_cell_position["max_row"] = max_row
   
   # 셀 데이터와 사용자가 설정한 데이터가(selectors)맞으면 해당 셀의 위치를 valid_cell_position에 저장
   for data in row_data:
     str_data = str(data.value).replace(" ","").replace("\n","")
-    print(data, data.__class__.__name__, str_data)
 
     for key, value in selectors.selectors.items():
       if (str_data in value):
-        if (key == "품명" or key == "가격"):
+        if (key == "name" or key == "price"):
           valid_cell_position[key].append(set_data(data))
           break
         valid_cell_position[key] = set_data(data)
@@ -52,18 +56,18 @@ def selector_cell(row_data: MutableSequence, name: str):
       
       # 품명같은 경우 합쳐진 셀들또한 유효한 값이 될 수 있음
       elif (data.__class__.__name__ == "MergedCell"):
-        valid_cell_position["품명"].append(set_data(data))
+        valid_cell_position["name"].append(set_data(data))
         break
 
   # 유효한 행 데이터가 아닐시(valid_cell_position데이터가 불충분)
-  if (valid_cell_position["가격"] == []):
+  if (valid_cell_position["price"] == []):
     return False
   
   # cj, 오뚜기, 청정원 --> 상품코드가 존재(품명 왼쪽셀)
   if (name == 'cjfreshway' or name == '오뚜기' or name == 'daesang'):
-    column, row = expressions.findall(valid_cell_position["품명"][0])
+    column, row = expressions.findall(valid_cell_position["name"][0])
     codenum_column = chr(ord(column) - 1)
-    valid_cell_position["코드번호"] = codenum_column + row
+    valid_cell_position["code_num"] = codenum_column + row
     
     
   
