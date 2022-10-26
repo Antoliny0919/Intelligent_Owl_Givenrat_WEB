2de3d7437c1823f2a004f8212ccc79d2a1463314
antoliny <antoliny0919@naver.com>
2022-10-26 16:36:20 +0900

    Create front-end data join membership page use react

---
+++ front_end_data/join_membership_page/src/JoinMembership/JoinMembershipBase.jsx
@@ -0,0 +1,13 @@
+import JoinMembershipFormArea from './components/JoinMembershipFormArea';
+import './css/JoinMembershipBase.css';
+
+export default function JoinMembershipBase() {
+  
+  return (
+    <div id="join-membership-page-layout-container">
+      <div id="sizing-block">
+        <JoinMembershipFormArea></JoinMembershipFormArea>
+      </div>
+    </div>
+  )
+}
---
+++ front_end_data/join_membership_page/src/JoinMembership/components/JoinMembershipFormArea.jsx
@@ -0,0 +1,9 @@
+import '../css/JoinMembershipFormArea.css';
+
+export default function JoinMembershipFormArea() {
+  return (
+    <div id="join-membership-form-area">
+
+    </div>
+  )
+}
