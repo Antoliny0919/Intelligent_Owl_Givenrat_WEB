4550828a7c67585b5b975900e200be511b43bcca
antoliny <antoliny0919@naver.com>
2022-11-23 11:13:08 +0900

    joinmembership page input block keyword area css update position

--- front_end_data/join_membership_page/src/JoinMembership/components/InputKeyWordArea.jsx
+++ front_end_data/join_membership_page/src/JoinMembership/components/InputKeyWordArea.jsx
@@ -3,7 +3,7 @@ import '../css/InputKeyWordArea.css';
 export default function InputKeyWordArea({ keyWord, attentionBlock}) {
   return (
     <div className="keyword-area">
-      <label className="input-key-word">{keyWord}</label>
+      <span className="input-key-word">{keyWord}</span>
       {/* 필수 입력사항에 대한 표시 --> attentionBlock */}
       {attentionBlock && <span className='attention-marker'>*</span>}
     </div>
