1f733cc5803b54ca35e7feeef15dafa016a5ffef
antoliny <antoliny0919@naver.com>
2022-11-28 17:30:30 +0900

    joinmembership page address detail input area information guide text update

--- front_end_data/join_membership_page/src/JoinMembership/components/DetailAddressInputArea.jsx
+++ front_end_data/join_membership_page/src/JoinMembership/components/DetailAddressInputArea.jsx
@@ -1,7 +1,6 @@
 import { useRecoilValue } from 'recoil';
 import { againSearch, state } from '../../Atom';
 
-import InputKeyWordArea from './InputKeyWordArea';
 import '../css/DetailAddressInputArea.css';
 import { Fragment } from 'react';
 
@@ -16,15 +15,17 @@ export default function DetailAddressInputArea() {
     // inputState 주소검색기능을 성공적으로 완료했을시 true 변환(상세주소 입력칸이 나타나게 됨)
     <div>
       {inputState ?
-      <div className='member-data-input-area detail-hide'>
+      <div className='member-data-input-area detail-hide info-true'>
         <div className='blank-area'></div>
         <div className='input-container'>
           <input className="input-area" id='detail-address' type={compress.inputType} autoComplete="on"></input> 
         </div>
       </div>
       : <Fragment></Fragment>}
-      <div className='detail-hide'>
-        <h1>*상세주소를 입력해주세요</h1>
+
+      <div className='information-guide-area detail-hide'>
+        <div className='blank-area'></div>
+        <span className='information-guide'>* 상세주소를 입력해주세요 *</span>
       </div>
     </div>
 
