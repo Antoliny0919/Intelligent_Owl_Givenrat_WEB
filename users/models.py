d1a96790999447df4e515303cab1d45aa2f8d3ac
antoliny <antoliny0919@naver.com>
2022-12-04 09:47:09 +0900

    User model email field update and fax_number, company_address, gender, birthdate field append

--- users/models.py
+++ users/models.py
@@ -4,9 +4,26 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 class User(AbstractUser):
-  company_name = models.CharField(max_length=100, null=True)
-  company_phone_number = PhoneNumberField(unique=True, null=True)
-  fax_number = PhoneNumberField(blank=True, null=True)
+  MAN = 'MAN'
+  WOMAN = 'WOMAN'
+  NOT_CHOICE = 'X'
+  
+  GENDER_CHOICES = [
+    (MAN, 'Man'),
+    (WOMAN, 'Woman'),
+    (NOT_CHOICE, ' '),
+  ]
+
+  email = models.EmailField(("email address"), max_length=150, default="")
+  company_name = models.CharField(max_length=100, default="")
+  company_phone_number = PhoneNumberField(default="")
+  fax_number = PhoneNumberField(blank=True, default="")
+  company_address = models.CharField(max_length=256, default="")
+  
+  # gender & birthdate 는 필수 입력 사항 x --> null = True
+  gender = models.CharField(max_length= 10, choices=GENDER_CHOICES, default=NOT_CHOICE, null=True)
+  birthdate = models.CharField(max_length=8, null=True)
+  
   
   
 
