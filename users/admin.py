cb4893971416b8993c344a4cefdbc8ad6b8bf9ee
antoliny <antoliny0919@naver.com>
2022-12-10 18:30:00 +0900

    users app UserAdmin class customizing fieldsets(admin site change page)

--- users/admin.py
+++ users/admin.py
@@ -1,21 +1,54 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import (
+    AdminPasswordChangeForm,
+    UserChangeForm,
+    UserCreationForm,
+)
 from .models import User
 
 # Register your models here.
 
 class CustomUserAdmin(UserAdmin):
-
+    
+    add_form_template = "admin/auth/user/add_form.html"
+    change_user_password_template = None
+    
+    # Change Userpage to be displayed field(admin site)
+    fieldsets = (
+        (None, {"fields": ("username", "password"),}),
+        (("Personal info"), {"fields": ("email", "gender", "birthdate"),}),
+        (("Company Info"), {"fields": ("company_name", "company_phone_number", "fax_number", "company_address")}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
     add_fieldsets = (
         (
             None,
             {
                 "classes": ("wide",),
-                # admin페이지 user 추가시 입력사항 form 추가
-                "fields": ("username", "password1", "password2", "email", "company_name", "company_phone_number",
-                "fax_number", "company_address", "gender", "birthdate"),
+                "fields": ("username", "password1", "password2"),
             },
         ),
+    )
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("username", "first_name", "last_name", "email")
+    ordering = ("username",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
     )
 
 
