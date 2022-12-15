7c31fb5e9d4b44dc0c0e9801f65f98cb9b4ea991
antoliny <antoliny0919@naver.com>
2022-12-15 19:39:12 +0900

    front-end homepage content area color change

--- front_end_data/homepage_main/src/index.js
+++ front_end_data/homepage_main/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MainBase></MainBase>
-    {/* <ContentBase></ContentBase> */}
+    <ContentBase></ContentBase>
   </React.StrictMode>
 );
 
