af20f857ad45478dcbf2d6256653b9d90f10c9e4
antoliny <antoliny0919@naver.com>
2022-12-18 11:15:31 +0900

    homepage banner area update

--- front_end_data/homepage_main/src/Main/components/BannerContent.jsx
+++ front_end_data/homepage_main/src/Main/components/BannerContent.jsx
@@ -5,6 +5,8 @@ export default function BannerContent({ BannerImg }) {
   return (
     <div id='banner-content-area'>
       <img src={BannerImg} alt="no_img" id="banner-img"></img>
+      <h1 id="banner-text">기벤라트는 당신의 업무를 효율적으로 해결해줍니다.</h1>
+      <button id="banner-start-button">시작하기</button>
     </div>
   )
 }
