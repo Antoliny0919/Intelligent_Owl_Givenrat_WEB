d054043c590ae3cd6f67cb39a400dd165f4d08b5
antoliny <antoliny0919@naver.com>
2023-01-09 17:42:33 +0900

    users app role separate to login app

--- login/views.py
+++ login/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def loginpage(request):
+  return render(request, 'users/login_page/build/login_page.html')
+
--- users/views.py
+++ users/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-
-def loginpage(request):
-  return render(request, 'users/login_page/build/login_page.html')
